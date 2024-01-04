475666efec242a7d06716259cb1fc8ddee5c8fa5
baranero <jakub.baran2@gmail.com>
2024-01-04 20:44:11 +0100

    individual category screen

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -11,6 +11,7 @@ const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("dddf65e5-ab60-4519-9001-6f8742ff0cf8");
 
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
